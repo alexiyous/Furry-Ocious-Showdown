1e4a0936bc09d7d27135fab2ef2112b2717cec25
QaorVa <100664948+QaorVa@users.noreply.github.com>
2023-10-21 18:28:21 +0700

    Enemy AK animation

--- Assets/Scripts/Enemy/Base/Enemy.cs
+++ Assets/Scripts/Enemy/Base/Enemy.cs
@@ -14,7 +14,7 @@ public class Enemy : MonoBehaviour, IDamageable, IEnemyMovable, ITriggerCheckabl
     public float currentSlowTime { get; set; }
     [field: SerializeField] public int score { get; set; }
     public Rigidbody2D RB { get; set; }
-    public bool isFacingRight { get; set; } = true;
+    public bool isFacingLeft { get; set; } = true;
     public bool isAggroed { get; set; }
     public bool isWithinAttackingRadius { get; set; }
     public bool isAlive { get; set; }
@@ -237,16 +237,17 @@ public class Enemy : MonoBehaviour, IDamageable, IEnemyMovable, ITriggerCheckabl
 
     public void CheckForDirection(Vector2 velocity)
     {
-        if(isFacingRight && velocity.x < 0f)
-        {
-            Vector3 rotator = new Vector3(transform.rotation.x, 180f, transform.rotation.z);
-            transform.rotation = Quaternion.Euler(rotator);
-            isFacingRight = !isFacingRight;
-        } else if(!isFacingRight && velocity.x > 0f)
+        if (isFacingLeft && velocity.x > 0f)
         {
             Vector3 rotator = new Vector3(transform.rotation.x, 0f, transform.rotation.z);
             transform.rotation = Quaternion.Euler(rotator);
-            isFacingRight = !isFacingRight;
+            isFacingLeft = false;
+        }
+        else if (!isFacingLeft && velocity.x < 0f)
+        {
+            Vector3 rotator = new Vector3(transform.rotation.x, 180f, transform.rotation.z);
+            transform.rotation = Quaternion.Euler(rotator);
+            isFacingLeft = true;
         }
     }
 
