4784e78c714a8087badfaa9dba565745f43c7ddc
alexiyous <alexiusrenanta@gmail.com>
2023-10-23 03:38:38 +0700

    MachineGun Tower Fully Animate

--- Assets/Scripts/MachinegunTower.cs
+++ Assets/Scripts/MachinegunTower.cs
@@ -1,11 +1,15 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
+
+
+
 public class MachinegunTower : Tower
 {
-    public Transform[] firingPoints;
+    public List<Transform> firingPoints;
 
     public override void UpgradeTower()
     {
@@ -27,7 +31,11 @@ public class MachinegunTower : Tower
                     baseObjects[0].SetActive(true);
                     rotationObjects[0].SetActive(true);
                     turretRotationPoint = rotationObjects[0].transform;
-                    firingPoint = firingP[0];
+                    firingPoints.Clear();
+                    for (int i = 0; i <= 2; i++)
+                    {
+                        firingPoints.Add(firingP[i]);
+                    }
                     cost = towerUpgrade[0].cost;
                     bps = bps2;
                     upgradeButtonText.text = cost.ToString();
@@ -38,7 +46,11 @@ public class MachinegunTower : Tower
                     baseObjects[1].SetActive(true);
                     rotationObjects[1].SetActive(true);
                     turretRotationPoint = rotationObjects[1].transform;
-                    firingPoint = firingP[1];
+                    firingPoints.Clear();
+                    for (int i = 3; i <= 5; i++)
+                    {
+                        firingPoints.Add(firingP[i]);
+                    }
                     bps = bps3;
                     upgradeNotif.SetActive(false);
                     break;
@@ -48,7 +60,7 @@ public class MachinegunTower : Tower
 
     public override void Shoot()
     {
-        int randomIndex = Random.Range(0, firingPoints.Length);
+        int randomIndex = UnityEngine.Random.Range(0, firingPoints.Count);
         GameObject bulletObj = Instantiate(bulletPrefab, firingPoints[randomIndex].position, turretRotationPoint.rotation);
         TowerBullet bulletScript = bulletObj.GetComponent<TowerBullet>();
         bulletScript.SetTarget(target);
