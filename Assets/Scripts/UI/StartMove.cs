47b2757857ff648ec37dca99f162dfd4a261ceb1
QaorVa <100664948+QaorVa@users.noreply.github.com>
2023-10-25 21:40:27 +0700

    Credit Scene

---
+++ Assets/Scripts/UI/StartMove.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+
+public class StartMove : MonoBehaviour
+{
+    public float speed = 50f;
+    public Vector2 initialPosition;
+    public Vector2 finalPosition;
+
+    public Ease easeType = Ease.InQuad;
+
+    RectTransform rectTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+
+        float distance = Vector2.Distance(initialPosition, finalPosition);
+        float duration = distance / speed;
+
+        // Use DoTween to move the UI element
+        rectTransform.anchoredPosition = initialPosition; // Set the initial position
+        rectTransform.DOAnchorPos(finalPosition, duration).SetEase(easeType);
+
+    }
+
+}
